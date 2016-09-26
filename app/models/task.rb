18df2ed870f2d64d6dd10946d1b1e0db60a2dac6
Alan Kebert <adkebert@yahoo.com>
2016-09-26 14:35:59 -0400

    validates that estimated duration is a positive integer

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,7 @@
 class Task < ApplicationRecord
   belongs_to :user
   validates :title, :description, :estimated_duration, presence: true
+  validates :estimated_duration, numericality: { only_integer: true, greater_than: 0 }
 
   enum time_sensitive: [ :schedulable, :not_schedulable ]
 end
