ff140eff196d1476e22c4feea1d8efa16d90e79a
Alan Kebert <adkebert@yahoo.com>
2016-10-05 14:23:49 -0400

    updated redirect to new surge url

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -24,7 +24,7 @@ class AuthenticationController < ApplicationController
 
     # Change this url to where Brett would like to capture the token
     # redirect_to "http://localhost:3000/?user_name=#{user_info[:display_name]}&token=#{URI.escape(token)}"
-    redirect_to "http://cuddly-guitar.surge.sh/?user_name=#{user_info[:display_name]}&token=#{URI.escape(token)}"
+    redirect_to "http://streamline.surge.sh/?user_name=#{user_info[:display_name]}&token=#{URI.escape(token)}"
   end
 
   def root
